fdfb4dedfabf97d59a87d3249ad05c6b8a382cff
lionman <leutul69@gmail.com>
2025-08-26 14:05:17 +0300

    cleanup

--- TodoApp/src/TodoWeb/Program.cs
+++ TodoApp/src/TodoWeb/Program.cs
@@ -1,51 +1,16 @@
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add services to the container.
-// builder.Services.AddControllersWithViews();
-
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
-
-// app.UseHttpsRedirection();
-// app.UseRouting();
-
-// app.UseAuthorization();
-
-// app.MapStaticAssets();
-
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "{controller=Home}/{action=Index}/{id?}")
-//     .WithStaticAssets();
-
-
-// app.Run();
-using Microsoft.AspNetCore.Mvc;
 using TodoWeb.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<ITodoRepository, InMemoryTodoRepository>();
 
-
 var app = builder.Build();
-
 
 app.UseStaticFiles();
 app.UseRouting();
 
-
-app.MapDefaultControllerRoute();
-
+// Only TodosController is needed; root path handled there
+app.MapControllers();
 
 app.Run();
