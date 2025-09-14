5abfc4569bde6e26066f5a01be780efafa6e8346
lionman <leutul69@gmail.com>
2025-09-14 19:44:52 +0000

    make tailwind run

---
+++ TodoApp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.cshtml',
+    './src/**/*.razor',
+    './src/**/*.html',
+    './src/**/*.cs',
+    './src/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
